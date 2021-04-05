2c965e85196651cbd966e203d0dbd7ba51c4e0ab
vvo <ex3me@ti-it.net>
2021-04-05 03:40:45 +0300

    task2
    1. Добавить в проект несколько контроллеров. Создать минимум 4 страницы. К примеру:
    a. Страницу приветствия.
    b. Страницу категорий новостей.
    c. Страницу вывода новостей по конкретной категории.
    d. Страницу вывода конкретной новости.
    e. Страницу авторизации.
    f. Страницу добавления новости.

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class LoginController extends Controller
+{
+public function index(){
+    echo "Please log in, or register";
+    exit;
+}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\NewsController;
+use \App\Http\Controllers\HelloController;
+use App\Http\Controllers\Admin\AdminNewsController;
+use \App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', [HelloController::class, 'index']);
+
+Route::get('/news', [NewsController::class, 'index']);
+Route::get('/news/card/{id}', [NewsController::class, 'card'])
+->where('id', '[0-9]+');
+Route::get('/news/category/{name}', [NewsController::class, 'category'])
+    ->where('name', '[a-z]+');
+Route::get('/news/category/{name}/news', [NewsController::class, 'categoryNews'])
+    ->where('name', '[a-z]+');
+
+Route::get('/login', [LoginController::class, 'index']);
+
+/** Админка новостей */
+Route::group([
+    'prefix' => '/admin/news',
+    'as' => 'admin::news::',
+], function () {
+    Route::get('/', [AdminNewsController::class, 'index'] )
+        ->name('index');
+    Route::get('/create',[AdminNewsController::class, 'create'])
+        ->name('create');
+    Route::get('/update',[AdminNewsController::class, 'update'])
+        ->name('update');
+    Route::get('/delete',[AdminNewsController::class, 'delete'])
+        ->name('delete');
 });
