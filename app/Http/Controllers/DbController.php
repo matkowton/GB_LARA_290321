e901a3e433bd92a7f5c079f4149646f3d86f5b1d
matkowton <matkowton@gmail.com>
2021-04-20 13:06:21 +0300

    lesson_5

---
+++ app/Http/Controllers/DbController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \DB;
+
+class DbController extends Controller
+{
+    public function index()
+    {
+       /* $sql = "
+            CREATE TABLE test (
+                id int PRIMARY KEY AUTO_INCREMENT,
+                content varchar(50)
+            )
+        ";
+        dd(DB::unprepared($sql));*/
+
+      /*  $sql = "INSERT INTO test (content) VALUES (:content)";
+        $result = DB::insert($sql, [':content' => 'test']);
+        dd($result);*/
+
+        /*$sql = "SELECT * FROM test WHERE id = :id";
+        $result = DB::select($sql, [':id' => 2]);
+        dd($result);*/
+
+
+        $result = DB::table('test')
+            ->where('id',3)
+            ->get();
+
+        dd($result);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::group([
         ->name('list');
 });
 
-
+Route::get('/db', [\App\Http\Controllers\DbController::class, 'index']);
 
 
 
